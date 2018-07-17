6fe191044a14f652c48e0daf04debdedbb900ee1
Yadunandan Batchu <nandubatchu@gmail.com>
2018-07-18 01:47:44 +0530

    symbol_to_description

--- generate.js
+++ generate.js
@@ -40,7 +40,20 @@ function symbol_to_details(json, dest_file_path) {
     });
 }
 
+function symbol_to_description(json, dest_file_path) {
+    obj = {}
+
+    json.forEach(token => {
+        obj[token.symbol] = token.description
+    });
+
+    jf.writeFile(dest_file_path, obj, {spaces: 4}, function(err) {
+        console.error(err)
+    });
+}
+
 var json_data = jf.readFileSync('tokens_data.json');
 symbol_to_contract(json_data, 'docs/mainnet/symbol_to_contract/index.json');
 contract_to_symbol(json_data, 'docs/mainnet/contract_to_symbol/index.json');
 symbol_to_details(json_data, 'docs/mainnet/symbol_to_details/index.json');
+symbol_to_description(json_data, 'docs/mainnet/symbol_to_description/index.json');
