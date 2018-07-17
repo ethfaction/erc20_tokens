4d15feb7d767386135e332a4793a3aa4fc5881fe
Yadunandan Batchu <nandubatchu@gmail.com>
2018-07-17 23:42:00 +0530

    travis push code

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b master
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/ethfaction/erc20_tokens.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master 
+}
+
+setup_git
+commit_website_files
+upload_files
