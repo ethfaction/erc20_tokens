c790f1d022ac4fca4faa7b9b77ac5551ad2c293e
Yadunandan Batchu <nandubatchu@gmail.com>
2018-07-18 01:08:51 +0530

    contract_to_symbol

--- generate.js
+++ generate.js
@@ -12,5 +12,18 @@ function symbol_to_contract(json, dest_file_path) {
     });
 }
 
+function contract_to_symbol(json, dest_file_path) {
+    obj = {}
+
+    json.forEach(token => {
+        obj[token.contract_address] = token.symbol
+    });
+
+    jf.writeFile(dest_file_path, obj, {spaces: 4}, function(err) {
+        console.error(err)
+    });
+}
+
 var json_data = jf.readFileSync('tokens_data.json');
 symbol_to_contract(json_data, 'docs/mainnet/symbol_to_contract/index.json');
+contract_to_symbol(json_data, 'docs/mainnet/contract_to_symbol/index.json');
