0b4f6af24d25711d6e74086e22712cc33601ca02
Yadunandan Batchu <nandubatchu@gmail.com>
2018-07-18 21:50:41 +0530

    update formats in npm module

--- npm_module/index.js
+++ npm_module/index.js
@@ -1,11 +1,16 @@
 var request = require("sync-request");
 exports.token_data = function(path) {
     var supported_paths = [
-        "symbol_to_details"
+        "symbol_to_details",
+        "symbol_to_contract",
+        "contract_to_symbol",
+        "symbol_to_description"        
     ]
     if (supported_paths.indexOf(path) >= 0) {
         var res = request('GET', 'https://ethfaction.github.io/erc20_tokens/mainnet/' + path);
         return res.getBody('utf-8');
     }
-    return "Not supported"
+    return {
+        "error": "format not supported"
+    }
 }
